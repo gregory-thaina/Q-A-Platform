2ae5a98b9785d23b5b7497df27ab0157bc1c897a
gregory-thaina <gregorygtes@gmail.com>
2022-06-20 21:29:24 -0300

    Inserção do body-parser para iniciar os trabalhos com a base de dados

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Setando o EJS
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Rotas desenvolvidas
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -14,7 +19,9 @@ app.get("/question", (req, res) => {
 });
 
 app.post("/savequestion", (req, res) => {
-    res.send("Received!")
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send("Title: " + title + "\nDescription: " + description)
 });
 
 app.listen(8080, () => {
